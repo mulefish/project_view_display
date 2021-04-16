4ec2a1cba9f191c57eed292adc1866608b7967dd
kermitt PDX <bluefishbrowndirt@gmail.com>
2021-04-16 17:25:07 -0500

    reading dirs

--- python/reportmaker.py
+++ python/reportmaker.py
@@ -3,7 +3,42 @@ import os
 import json
 from letters import getLetterFromNumber
 
-pattern = ".js"
-rootPath = "."
+pattern = "*.js"
+rootPath = "../src/"
 
-print(getLetterFromNumber(39))
+
+# class Node:
+#     def __init__(self, a, b, family, letter):
+#         self.a = a
+#         self.b = b
+#         self.family = family
+#         self.letter = letter
+
+#     def show(self):
+#         x = {
+#             "a": self.a,
+#             "b": self.b,
+#             "l": self.letter,
+#             "f": self.family
+#         }
+#         return json.dumps(x)
+
+
+def readFS_makeDotGraph():
+    nodes = []
+    k = 0
+    i = 0
+    for root, dirs, files in os.walk(rootPath):
+        root = root.replace(os.sep, "|")
+        obj = {}
+        obj["id"] = getLetterFromNumber(i)
+        obj["dir"] = root
+        # print("{}".format(obj))
+        i += 1
+        for filename in fnmatch.filter(files, pattern):
+            x = "{}|{}".format(root, filename)
+            print("{}     {}".format(i, x))
+
+
+if __name__ == "__main__":
+    readFS_makeDotGraph()
